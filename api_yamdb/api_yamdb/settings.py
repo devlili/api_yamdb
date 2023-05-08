3b22bb1bc9f7f7332558d0f7dfbd2db96e92325f
Лилия Альмухаметова <almuhametova@gmail.com>
2023-05-08 21:19:00 +0500

    Del UsersConfig

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -15,7 +15,6 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
