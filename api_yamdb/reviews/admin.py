f78cb7b950db4f769bfd030815bec21780b8a118
Лилия Альмухаметова <almuhametova@gmail.com>
2023-05-08 22:57:07 +0500

    Add models Review, Comment

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Comment, Review
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("pk", "text", "pub_date", "title", "review", "author")
+    search_fields = ("text",)
+    list_filter = ("pub_date",)
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ("pk", "text", "pub_date", "title", "author", "score")
+    search_fields = ("text",)
+    list_filter = ("pub_date",)
