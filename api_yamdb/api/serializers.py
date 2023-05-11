ac4f789bff7db5dc73d075fda594a6a175ffaad8
Лилия Альмухаметова <almuhametova@gmail.com>
2023-05-12 00:15:33 +0500

    Fix models, views, ser

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -2,29 +2,28 @@ from django.db.models import Avg
 from django.utils import timezone
 from rest_framework import serializers
 from rest_framework.relations import SlugRelatedField
-from rest_framework.validators import UniqueTogetherValidator
-from reviews.models import Categories, Comment, Genres, Review, Titles
+from reviews.models import Category, Comment, Genre, Review, Title
 
 
-class GenresSerializer(serializers.ModelSerializer):
+class GenreSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Genres
-        fields = "__all__"
+        model = Genre
+        fields = ("name", "slug")
 
 
-class CategoriesSerializer(serializers.ModelSerializer):
+class CategorySerializer(serializers.ModelSerializer):
     class Meta:
-        model = Categories
-        fields = "__all__"
+        model = Category
+        fields = ("name", "slug")
 
 
 class TitleReadSerializer(serializers.ModelSerializer):
     rating = serializers.SerializerMethodField()
-    genre = GenresSerializer(read_only=True, many=True)
-    category = CategoriesSerializer(read_only=True)
+    genre = GenreSerializer(read_only=True, many=True)
+    category = CategorySerializer(read_only=True)
 
     class Meta:
-        model = Titles
+        model = Title
         fields = (
             "id",
             "name",
@@ -36,7 +35,7 @@ class TitleReadSerializer(serializers.ModelSerializer):
         )
 
     def get_rating(self, obj):
-        rating = obj.reviews.aggregate(Avg('score')).get('score__avg')
+        rating = obj.reviews.aggregate(Avg("score")).get("score__avg")
         if not rating:
             return rating
         return round(rating, 1)
@@ -44,14 +43,14 @@ class TitleReadSerializer(serializers.ModelSerializer):
 
 class TitleCreateSerializer(serializers.ModelSerializer):
     genre = serializers.SlugRelatedField(
-        many=True, slug_field="slug", queryset=Genres.objects.all()
+        many=True, slug_field="slug", queryset=Genre.objects.all()
     )
     category = serializers.SlugRelatedField(
-        slug_field="slug", queryset=Categories.objects.all()
+        slug_field="slug", queryset=Category.objects.all()
     )
 
     class Meta:
-        model = Titles
+        model = Title
         fields = (
             "id",
             "name",
@@ -65,7 +64,7 @@ class TitleCreateSerializer(serializers.ModelSerializer):
         current_year = timezone.now().year
         if not 0 <= value <= current_year:
             raise serializers.ValidationError(
-                'Проверьте год создания произведения.'
+                "Проверьте год создания произведения."
             )
         return value
 
@@ -99,10 +98,20 @@ class ReviewSerializer(serializers.ModelSerializer):
         model = Review
         # read_only_fields = ("id", "author", "pub_date")
 
-        validators = [
-            UniqueTogetherValidator(
-                queryset=Review.objects.all(),
-                fields=("author", "title"),
-                message="Вы уже оставили отзыв на это произведение.",
+    def validate(self, data):
+        if self.context.get("request").method != "POST":
+            return data
+        title = self.context.get("view").kwargs.get("title_id")
+        author = self.context.get("request").user
+        if Review.objects.filter(author=author, title=title).exists():
+            raise serializers.ValidationError(
+                "Вы уже написали отзыв к этому произведению."
             )
-        ]
+        return data
+
+    def validate_score(self, value):
+        if not 1 <= value <= 10:
+            raise serializers.ValidationError(
+                'Оценкой может быть целое число в диапазоне от 1 до 10.'
+            )
+        return value
--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,9 +1,9 @@
 from api.views import (
-    CategoriesViewSet,
+    CategoryViewSet,
     CommentViewSet,
-    GenresViewSet,
+    GenreViewSet,
     ReviewViewSet,
-    TitlesViewSet,
+    TitleViewSet,
 )
 from django.urls import include, path
 from rest_framework import routers
@@ -11,12 +11,13 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+
 app_name = "api"
 
 router_v1 = routers.DefaultRouter()
-router_v1.register("categories", CategoriesViewSet, basename="categories")
-router_v1.register("genres", GenresViewSet, basename="genres")
-router_v1.register("titles", TitlesViewSet, basename="titles")
+router_v1.register("categories", CategoryViewSet, basename="categories")
+router_v1.register("genres", GenreViewSet, basename="genres")
+router_v1.register("titles", TitleViewSet, basename="titles")
 router_v1.register(
     r"titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments",
     CommentViewSet,
@@ -28,6 +29,6 @@ router_v1.register(
 
 urlpatterns = [
     path("v1/", include(router_v1.urls)),
-    path('api/auth/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/', TokenRefreshView.as_view(), name='token_refresh'),
+    path("api/auth/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
