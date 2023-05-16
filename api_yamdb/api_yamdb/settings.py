8b1744759061cb4bdf8775802c6372c65bf26e13
Лилия Альмухаметова <almuhametova@gmail.com>
2023-05-16 20:50:55 +0500

    Revert "Merge branch 'master' of github.com:devlili/api_yamdb into develop"
    This reverts commit 676e826bd8c90a568e931bd47c9deb575bb4c63d, reversing
    changes made to 03af565bc4170c7b4b0c8244c1e0bd71ce791533.

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -48,7 +48,7 @@ TEMPLATES_DIR = BASE_DIR / 'templates'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
